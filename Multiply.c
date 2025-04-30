bc5f97958e7e2886a3b46f2626be9139b41d54a8
Masrafi404 <mdmasrafi902@gmail.com>
2025-04-30 22:22:35 +0600

    multiply

---
+++ Multiply.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* get value from user */
+    long long int A, B;
+    scanf("%lld %lld", &A, &B);
+    
+    // print the multiplication
+    printf("%lld", A * B);
+    return 0;
+}
